8e043bf23b57209cf091c9e6db07a1cb1c6fe444
shortdaddy0711 <sodoatm@gmail.com>
2020-11-05 00:52:28 -0800

    Implement conditional rendering

--- client/app.jsx
+++ client/app.jsx
@@ -1,99 +1,150 @@
-import React    from 'react';
-import ReactDOM from 'react-dom';
-import axios    from 'axios';
-
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      form: ''
-    }
+    this.state = {form: ''};
+
+    this.handleFormState = this.handleFormState.bind(this);
   }
 
+  componentDidMount() {
+    this.setState({form: 'Checkout'})
+  }
 
+  handleFormState(destination) {
+    this.setState({form: destination});
+  }
 
   render() {
+    const destination = this.state.form;
+    let form;
+    if (destination === 'Checkout') { form = <Checkout nextForm={this.handleFormState} />};
+    if (destination === 'Form1') { form = <Form1 nextForm={this.handleFormState} />};
+    if (destination === 'Form2') { form = <Form2 nextForm={this.handleFormState} />};
+    if (destination === 'Form3') { form = <Form3 nextForm={this.handleFormState} />};
+    if (destination === 'Confirmation') { form = <Confirmation nextForm={this.handleFormState} />};
+
     return (
-      <div >
+      <div className="container">
         <div>Multistep Checkout</div>
-          <div><button type='button' >Checkout</button></div>
-
-          <div><Form1 /></div>
-          <div><Form2 /></div>
-          <div><Form3 /></div>
-          <div><Confirmation /></div>
-
+        {form}
       </div>
     )
   }
 }
 
-class Form1 extends React.Compoent {
+class Checkout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {nextForm: ''};
+  }
+
+  componentDidMount() {
+    this.setState({nextForm: 'Form1'})
+  }
+
+
+  render() {
+    return (
+      <form>
+        <button
+          className="btn"
+          id="checkout"
+          type='button'
+          onClick={(e) => this.props.nextForm(this.state.nextForm)}
+        >Checkout</button>
+      </form>
+    )
+  }
+}
+
+class Form1 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      form: null,
+      account: {
+        name:'',
+        email:'',
+        password:''
+      }
+    };
+  }
 
+  componentDidMount() {
+    this.setState({form: 'Form2'})
+  }
+
+  render() {
+    return (
+      <form>
+        <div>
+          <label >name: </label>
+          <input type="text" name="name" id="name" required />
+        </div>
+        <div>
+          <label >email: </label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div>
+          <label >password: </label>
+          <input type="password" name="password" id="password" required />
+        </div>
+        <div>
+          <button
+            className="btn"
+            id="toForm2"
+            type='button'
+            onClick={(e) => this.props.nextForm(this.state.form)}
+          >Next</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+class Form2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: this.props.nextForm
+    }
+  }
+
+  render() {
+    return (
+      <button className="btn" id="next" type='button' onClick={(e) => this.setState({form: <Form3 nextForm={this.state.form}/>})}>Next</button>
+    )
+  }
+}
+
+class Form3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: this.props.nextForm
     }
   }
 
 
   render() {
     return (
-      <div >
-      </div>
+      <button className="btn" id="next" type='button' onClick={(e) => this.setState({form: <Confirmation nextForm={this.state.form}/>})}>Next</button>
     )
   }
 }
 
-class Form2 extends React.Compoent {
+class Confirmation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      form: this.props.nextForm
     }
   }
 
 
   render() {
     return (
-      <div >
-      </div>
-    )
-  }
-}
-
-class Form3 extends React.Compoent {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-
-  render() {
-    return (
-      <div >
-      </div>
-    )
-  }
-}
-
-class Confirmation extends React.Compoent {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-
-  render() {
-    return (
-      <div >
-      </div>
+      <button className="btn" id="next" type='button' onClick={(e) => this.setState({form: <Checkout nextForm={this.state.form}/>})}>Purchase</button>
     )
   }
 }
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+// Import express
 const express = require('express');
 const path = require('path');
+
+// Middleware
+const parser = require('body-parser');
+
+// Create our app
 const app = express();
+
+//  Create a port variable
 const PORT = 3000;
 
+//  2. Implement middleware with use method and run express static
 app.use(express.static('public'))
+app.use(parser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
